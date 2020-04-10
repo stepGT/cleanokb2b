9c8ff38f3f54881f36164ed051e2be9cb55571d8
stepGT <stepgt@mail.ru>
2020-04-10 18:01:28 +1000

    Added show/hide top_header on hover

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
 import './assets/styles/style.scss';
 import './bootstrap/bootstrap';
 
-$(document).ready(function ($) {});
+$(document).ready(function ($) {
+  /**
+   *
+   */
+  function __top_header_show() {
+    $(this).remove();
+    $('.top_header').show();
+    $('.header__logo, .header__banner nav').hide();
+  }
+
+  $('.top_header__block').hover(__top_header_show);
+
+  $('.top_header').hover(function (e) {
+    if (e.type === 'mouseleave') {
+      $('body .container.wrapper').prepend('<div class="top_header__block"></div>');
+      $('.top_header__block').hover(__top_header_show);
+      $('.top_header').hide();
+      $('.header__logo, .header__banner nav').show();
+    }
+  });
+});
