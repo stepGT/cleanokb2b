aa94f1c921419cb90d7a72411c7f7766265e8127
stepGT <stepgt@mail.ru>
2020-04-12 18:54:25 +1000

    Added event onclick tabcontent

--- src/index.js
+++ src/index.js
@@ -2,6 +2,18 @@ import './assets/styles/style.scss';
 import './bootstrap/bootstrap';
 
 $(document).ready(function ($) {
+  $('.services__tabs a').on('click', function (e) {
+    e.preventDefault();
+    let service_link = $(this).data('service-link');
+    // Get all elements with class='services__tab_content_item' and hide them
+    $('.services__tab_content_item').hide();
+    // Get all a elements with class="services__tab_link" and remove the class 'active'
+    $('a.services__tab_link').removeClass('active');
+    // Show the current tab, and add an 'active' class to the link that opened the tab
+    $(this).addClass('active');
+    $('#' + service_link).show();
+  });
+
   /**
    *
    */
